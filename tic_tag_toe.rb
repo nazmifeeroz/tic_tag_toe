4b709a394d307d5e38a893113b02a67499872e5c
sha <urabummer4@hotmail.com>
2017-08-21 17:01:48 +0800

    latest

--- tic_tag_toe.rb
+++ tic_tag_toe.rb
@@ -1,9 +1,15 @@
 # tic-tag-toe game
 
-puts "Play against player(p) or computer(c)?"
-pc = gets.chomp
-
+# puts "Play against player(p) or computer(c)?"
+# pc = gets.chomp
+puts "\e[H\e[2J"
+name = []
 play = "y"
+
+print "Player 1 name: "
+name[0] = gets.chomp.upcase
+print "Player 2 name: "
+name[1] = gets.chomp.upcase
 
 while play == "y" do
 
@@ -11,25 +17,12 @@ while play == "y" do
 
 	(1..9).each { |n| a[n-1] = n }
 
-	play = "1"
+	play = 0
 	game = 0
 	error = ""
 	allmoves = 0
 
-	#ask for user name
-
 	while game <= 9 do 
-
-		puts "\e[H\e[2J"
-		puts "\tTic-Tag-Toe"
-		puts "\t-----------\n\n"
-		puts "\t #{a[0]} | #{a[1]} | #{a[2]}"
-		puts "\t ---------"
-		puts "\t #{a[3]} | #{a[4]} | #{a[5]}"
-		puts "\t ---------"
-		puts "\t #{a[6]} | #{a[7]} | #{a[8]}\n\n"
-		puts "Player #{play} Select a number:"
-		puts error
 
 		if a[0] == "X" && a[3] == "X" && a[6] == "X"
 			puts "Player X wins!" 
@@ -88,16 +81,52 @@ while play == "y" do
 			break 
 		end
 
+		puts "\e[H\e[2J"
+		puts "\tIt's " + name[0] + " VS " + name[1] +"!!\n\n"
+
+		puts "\tTic-Tag-Toe"
+		puts "\t-----------\n\n"
+		puts "\t #{a[0]} | #{a[1]} | #{a[2]}"
+		puts "\t ---------"
+		puts "\t #{a[3]} | #{a[4]} | #{a[5]}"
+		puts "\t ---------"
+		puts "\t #{a[6]} | #{a[7]} | #{a[8]}\n\n"
+		puts "Enter 12 to quit. 11 to reset game."
+		puts "#{name[play]} Enter Choice:"
 		choice = gets.chomp.to_i
+		puts error
+
+		
 		error = ""
 		check = 9
 
 		(0..8).each do |m|
 
 			if choice == a[m]
-				if play == "1" then a[m] = "X" else a[m] = "o" end
-				if play == "1" then play = "2" else play = "1" end
+				if play == 0 then a[m] = "X" else a[m] = "o" end
+				if play == 0 then play = 1 else play = 0 end
 				game += 1
+
+			elsif choice == 12
+				puts "Are you sure you wish to quit? (y/n)"
+				quitans = gets.chomp
+				if quitans == "y" then exit else break end
+
+			elsif choice == 11
+				puts "Restart? (y/n)"
+				restartansw = gets.chomp
+				if restartansw == "y"
+					(1..9).each { |n| a[n-1] = n }
+
+					play = 0
+					game = 0
+					error = ""
+					allmoves = 0
+					break
+				else 
+					break
+				end
+					
 			else
 				check = check - 1
 			end
