25739eddd07af9e0152b14c7bc21e35f2c9d0f6e
Nazmi <nazmifeeroz@hotmail.com>
2017-08-21 18:42:41 +0800

    All done

--- tic_tag_toe.rb
+++ tic_tag_toe.rb
@@ -1,4 +1,25 @@
 # tic-tag-toe game
+def winning_check(a, player_sym)
+
+	if a[0] == player_sym && a[3] == player_sym && a[6] == player_sym
+		return true
+	elsif a[0] == player_sym && a[1] == player_sym && a[2] == player_sym
+		return true
+	elsif a[0] == player_sym && a[4] == player_sym && a[8] == player_sym
+		 return true
+	elsif a[1] == player_sym && a[4] == player_sym && a[7] == player_sym
+		 return true
+	elsif a[3] == player_sym && a[4] == player_sym && a[5] == player_sym
+		 return true
+	elsif a[4] == player_sym && a[6] == player_sym && a[2] == player_sym
+		 return true
+	elsif a[5] == player_sym && a[8] == player_sym && a[2] == player_sym
+		return true
+	elsif a[6] == player_sym && a[7] == player_sym && a[8] == player_sym
+		return true
+	end
+end
+
 puts "\e[H\e[2J"
 puts "Play against player(p) or computer(c)?"
 pc = gets.chomp
@@ -25,54 +46,10 @@ if pc == "p"
 
 		while game <= 9 do 
 
-			if a[0] == "X" && a[3] == "X" && a[6] == "X"
+			if winning_check(a, "X")
 				puts "#{name[0]} wins!" 
 				break 
-			elsif a[0] == "X" && a[1] == "X" && a[2] == "X"
-				puts "#{name[0]} wins!" 
-				break 
-			elsif a[0] == "X" && a[4] == "X" && a[8] == "X"
-				puts "#{name[0]} wins!" 
-				break 
-			elsif a[1] == "X" && a[4] == "X" && a[7] == "X"
-				puts "#{name[0]} wins!" 
-				break 
-			elsif a[3] == "X" && a[4] == "X" && a[5] == "X"
-				puts "#{name[0]} wins!" 
-				break 
-			elsif a[4] == "X" && a[6] == "X" && a[2] == "X"
-				puts "#{name[0]} wins!" 
-				break 
-			elsif a[5] == "X" && a[8] == "X" && a[2] == "X"
-				puts "#{name[0]} wins!" 
-				break 
-			elsif a[6] == "X" && a[7] == "X" && a[8] == "X"
-				puts "#{name[0]} wins!" 
-				break 
-			end
-
-			if a[0] == "o" && a[3] == "o" && a[6] == "o"
-				puts "#{name[1]} wins!" 
-				break 
-			elsif a[0] == "o" && a[1] == "o" && a[2] == "o"
-				puts "#{name[1]} wins!" 
-				break 
-			elsif a[0] == "o" && a[4] == "o" && a[8] == "o"
-				puts "#{name[1]} wins!" 
-				break 
-			elsif a[1] == "o" && a[4] == "o" && a[7] == "o"
-				puts "#{name[1]} wins!" 
-				break 
-			elsif a[3] == "o" && a[4] == "o" && a[5] == "o"
-				puts "#{name[1]} wins!" 
-				break 
-			elsif a[4] == "o" && a[6] == "o" && a[2] == "o"
-				puts "#{name[1]} wins!" 
-				break 
-			elsif a[5] == "o" && a[8] == "o" && a[2] == "o"
-				puts "#{name[1]} wins!" 
-				break 
-			elsif a[6] == "o" && a[7] == "o" && a[8] == "o"
+			elsif winning_check(a, "o")
 				puts "#{name[1]} wins!" 
 				break 
 			end
@@ -146,6 +123,7 @@ if pc == "p"
 # -----------------------------------player vs comp--------------------
 else
 
+	x = 0
 	name = []
 	cont = "y"
 	puts "\e[H\e[2J"
@@ -233,6 +211,7 @@ else
 			puts "\t\t\t\t #{a[6]} | #{a[7]} | #{a[8]}\n\n\n"
 			puts "Enter 12 to quit. 11 to reset game.\n"
 			puts "\n\n#{error}\n\n"
+			puts "Computer chose #{x}"
 			print "#{name[play]} Enter Choice: "
 
 			if play == 0
@@ -281,13 +260,11 @@ else
 			elsif play == 1
 				
 				x = a.map {|x| Integer(x) rescue nil }.compact.sample
-				
-				puts "computer turn #{x}"
 				(0..8).each do |r|
 					if x == a[r] then a[r] = "o" end
 				end
 				play = 0
-					game += 1
+				game += 1
 			end
 			
 		end
